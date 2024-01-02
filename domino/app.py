41e05d618bf6e45c936851fb3d1f535974486e8c
Walter Barreto <112351885+walterbfilho@users.noreply.github.com>
2024-01-02 00:30:26 -0300

    rota registro

--- domino/app.py
+++ domino/app.py
@@ -71,18 +71,18 @@ login.login_view = 'login'
 def load_user(id):
     return User.query.get(int(id))
 
-#@app.route('/register', methods=['GET', 'POST'])
-#def register():
-#    if current_user.is_authenticated:
-#        return redirect(url_for('home'))
-#    form = RegistrationForm()
-#    if form.validate_on_submit():
-#        user = User(username=form.username.data, email=form.email.data)
-#        user.set_password(form.password.data)
-#        db.session.add(user)
-#        db.session.commit()
-#        return redirect(url_for('login'))
-#    return render_template('register.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
